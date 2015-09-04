665c67cc48ff415137907ff420fc6a4af18e5627
timjroberts <tim@timjroberts.com>
2015-09-04 13:26:48 +0100

    test that AmbientContext objects are disposed of

--- test/AmbientContextSpec.ts
+++ test/AmbientContextSpec.ts
@@ -7,12 +7,14 @@ import { using, getCurrentObject } from "../src/index";
  */
 class MockContext {
 	private _id: number;
+	private _isDisposed: boolean;
 
 	/**
 	 * Initializes a new instance of the mock context object.
 	 */
 	constructor() {
 		this._id = Math.floor(Math.random());
+		this._isDisposed = false;
 	}
 
 	/**
@@ -22,6 +24,22 @@ class MockContext {
 	 */
 	public get id(): number {
 		return this._id;
+	}
+
+	/**
+	 * Gets a flag indicating whether the object has been disposed.
+	 * 
+	 * @returns 'true' if the object has been diposed; 'false' otherwise.
+	 */
+	public get isDisposed(): boolean {
+		return this._isDisposed;
+	}
+
+	/**
+	 * Disposes of the current object.
+	 */
+	public dispose(): void {
+		this._isDisposed = true;
 	}
 
 	/**
@@ -38,13 +56,38 @@ class MockContext {
 describe("Ambient Context", () => {
 	describe("with single scope block", () => {
 		it("should allow retrieval of a context item", (done) => {
-			var mockObject = new MockContext();
+			let mockObject = new MockContext();
 
 			using([mockObject], () => {
 				expect(MockContext.current).to.not.be.undefined;
 				expect(MockContext.current.id).to.be.equal(mockObject.id);
 				done();
 			});
+		});
+
+		it("should dispose of the context item when the scope block goes out of scope", (done) => {
+			let mockObject = new MockContext();
+			let doneFlag = false;
+
+			let expectationsOnCompleteFunc = () => {
+				if (doneFlag) {
+					expect(mockObject.isDisposed).to.equal(true);
+
+					return done();
+				}
+
+				setImmediate(expectationsOnCompleteFunc);
+			};
+
+			expect(mockObject.isDisposed).to.equal(false);
+
+			using([mockObject], () => {
+				expect(mockObject.isDisposed).to.equal(false);
+
+				doneFlag = true;
+			});
+
+			setImmediate(expectationsOnCompleteFunc);
 		});
 	});
 });
